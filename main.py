db20cf0b323f957e4fd03e36f95bf243216716b0
Marcus Gajozo <marcusgajozo@gmail.com>
2024-06-03 17:24:14 -0400

    Class organization

--- main.py
+++ main.py
@@ -1,57 +1,97 @@
-class TicTacToe:
+class TicTacToeBoard:
     def __init__(self):
         self.board = [" " for _ in range(9)]
-        self.current_winner = None  # keep track of the winner
 
     def print_board(self):
-        # This method prints the board after every move
-        for row in [self.board[i * 3:(i + 1) * 3] for i in range(3)]:
+        for row in [self.board[i * 3 : (i + 1) * 3] for i in range(3)]:
             print("| " + " | ".join(row) + " |")
-
-    @staticmethod
-    def print_board_nums():
-        # This tells us what number corresponds to what box
-        number_board = [[str(i) for i in range(j * 3, (j + 1) * 3)] for j in range(3)]
-        for row in number_board:
-            print("| " + " | ".join(row) + " |")
-
-    def available_moves(self):
-        return [i for i, spot in enumerate(self.board) if spot == " "]
-
-    def empty_squares(self):
-        return " " in self.board
-
-    def num_empty_squares(self):
-        return self.board.count(" ")
 
     def make_move(self, square, letter):
         if self.board[square] == " ":
             self.board[square] = letter
-            if self.winner(square, letter):
+            return True
+        return False
+
+
+class TicTacToeGame:
+    def __init__(self):
+        self.board = TicTacToeBoard()
+        self.current_winner = None
+
+    def available_moves(self):
+        return [i for i, spot in enumerate(self.board.board) if spot == " "]
+
+    def empty_squares(self):
+        return " " in self.board.board
+
+    def check_winner(self, square, letter):
+        if self._check_row(square, letter):
+            return True
+        if self._check_column(square, letter):
+            return True
+        if self._check_diagonals(square, letter):
+            return True
+        return False
+
+    def _check_row(self, square, letter):
+        row_index = square // 3
+        row = self.board.board[row_index * 3 : (row_index + 1) * 3]
+        if all([spot == letter for spot in row]):
+            return True
+        return False
+
+    def _check_column(self, square, letter):
+        col_index = square % 3
+        column = [self.board.board[col_index + i * 3] for i in range(3)]
+        if all([spot == letter for spot in column]):
+            return True
+        return False
+
+    def _check_diagonals(self, square, letter):
+        if square % 2 == 0:
+            diagonal1 = [self.board.board[i] for i in [0, 4, 8]]
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            diagonal2 = [self.board.board[i] for i in [2, 4, 6]]
+            if all([spot == letter for spot in diagonal2]):
+                return True
+        return False
+
+    def make_move(self, square, letter):
+        if self.board.make_move(square, letter):
+            if self.check_winner(square, letter):
                 self.current_winner = letter
             return True
         return False
 
-    def winner(self, square, letter):
-        # check the row
-        row_ind = square // 3
-        row = self.board[row_ind * 3:(row_ind + 1) * 3]
-        if all([spot == letter for spot in row]):
-            return True
-        # check the column
-        col_ind = square % 3
-        column = [self.board[col_ind + i * 3] for i in range(3)]
-        if all([spot == letter for spot in column]):
-            return True
-        # check diagonals
-        if square % 2 == 0:
-            diagonal1 = [self.board[i] for i in [0, 4, 8]]
-            if all([spot == letter for spot in diagonal1]):
-                return True
-            diagonal2 = [self.board[i] for i in [2, 4, 6]]
-            if all([spot == letter for spot in diagonal2]):
-                return True
-        return False
+
+class TicTacToePlay:
+    def __init__(self, game, x_player, o_player):
+        self.game = game
+        self.x_player = x_player
+        self.o_player = o_player
+
+    def play_game(self, letter="X"):
+        self.game.board.print_board()
+
+        while self.game.empty_squares():
+            if letter == "O":
+                square = self.o_player.get_move(self.game)
+            else:
+                square = self.x_player.get_move(self.game)
+
+            if self.game.make_move(square, letter):
+                print(letter + f" makes a move to square {square}")
+                self.game.board.print_board()
+                print("")
+
+                if self.game.current_winner:
+                    print(letter + " wins!")
+                    return letter
+
+                letter = "O" if letter == "X" else "X"
+
+        print("It's a tie!")
 
 
 class Player:
@@ -81,38 +121,9 @@ class HumanPlayer(Player):
         return val
 
 
-def play(game, x_player, o_player, print_game=True):
-    if print_game:
-        game.print_board_nums()
-
-    letter = "X"  # starting letter
-    while game.empty_squares():
-        if game.num_empty_squares() == 0:
-            break
-        if letter == "O":
-            square = o_player.get_move(game)
-        else:
-            square = x_player.get_move(game)
-
-        if game.make_move(square, letter):
-            if print_game:
-                print(letter + f" makes a move to square {square}")
-                game.print_board()
-                print("")  # empty line
-
-            if game.current_winner:
-                if print_game:
-                    print(letter + " wins!")
-                return letter  # end the loop and exit
-
-            letter = "O" if letter == "X" else "X"  # switch player
-
-    if print_game:
-        print("It's a tie!")
-
-
 if __name__ == "__main__":
+    game = TicTacToeGame()
     x_player = HumanPlayer("X")
     o_player = HumanPlayer("O")
-    t = TicTacToe()
-    play(t, x_player, o_player, print_game=True)
+    game_controller = TicTacToePlay(game, x_player, o_player)
+    game_controller.play_game()
