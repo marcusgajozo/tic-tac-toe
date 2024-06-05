25c40cb285fa5332c68cba4fc4fbe884f5aa8604
Marcus Gajozo <marcusgajozo@gmail.com>
2024-06-05 13:36:23 -0400

    fix: Set attributes private for class of scoreboard

--- src/scoreboard.py
+++ src/scoreboard.py
@@ -1,16 +1,16 @@
 class Scoreboard:
     def __init__(self):
-        self.x = 0
-        self.o = 0
+        self._x = 0
+        self._o = 0
 
     def set_vitory(self, letter):
         if letter == "X":
-            self.x += 1
+            self._x += 1
         else:
-            self.o += 1
+            self._o += 1
 
     def show_scoreboard(self):
         print("--------------------")
-        print(f"X: {self.x}")
-        print(f"O: {self.o}")
+        print(f"X: {self._x}")
+        print(f"O: {self._o}")
         print("--------------------")
